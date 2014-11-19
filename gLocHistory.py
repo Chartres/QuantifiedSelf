9067c430aa69d02b69cf9b5678e6dd1fc92fd7a1
Palo Dravecky <palo.dravecky@gmail.com>
2014-11-19 16:54:45 +0100

    munge and export to json and csv

--- gLocHistory.py
+++ gLocHistory.py
@@ -1,27 +1,44 @@
+import sys
 import json
+import csv
+from time import strftime, gmtime
 
-def parseLocationHistory(filePath):
-	# converts Google location history json file to python list
-	with open(filePath):
-		data = json.load(data["locations"])
-		d = [a[u'latitudeE7'], a[u'longitudeE7'], a[u'timestampMs'] for a in data]
+def parseLocationHistory(inFilePath):
+	# grabs lat, lon and time from Google location history json file into python list
+	with open(inFilePath) as f:
+		print f
+		data = json.load(f)
+		data = data[u'locations']
+		d = [(a[u'latitudeE7'], a[u'longitudeE7'], a[u'timestampMs']) for a in data]
 	return d
 	
-def mungeForZenobase(data):
+def munge(data):
 	# converts lat, lon and timestamp from Google location history data list
 	# to zenobase import standard (https://zenobase.com/#/api/create-event)
 	zData = []
 	
 	for rec in data:
 		z = {}
-		z['lat'] = rec[0] * 0.0000001 #latitude
-		z['lon'] = rec[1] * 0.0000001 #longitude
-		z['time'] = strftime("%Y-%m-%dT%H:%M:%S", rec[2]/1000) #convert ms since epoch to yyyy-MM-dd'T'hh:mm:ss.SSSZ time
+		z['lat'] = round(rec[0] * 0.0000001, 7) #latitude
+		z['lon'] = round(rec[1] * 0.0000001, 7) #longitude
+		z['time'] = strftime("%Y-%m-%dT%H:%M:%S", gmtime(int(rec[2])/1000)) #convert ms since epoch to yyyy-MM-dd'T'hh:mm:ss.SSSZ time
 		zData.append(z)
+
+	return zData
 	
+def exportToZenobase(data):
 	return json.dumps(zData)
 	
-def googleLocationHistoryForZenobase(filePath):
+def exportToCSV(data, outFilePath):
+	with open(outFilePath, "wb") as f:
+		writer = csv.DictWriter(f, data[0].keys())
+		writer.writeheader()
+		writer.writerows(data)
+	
+if __name__ == '__main__':
 	#takes your Google location history takeout json file and formats it
 	#for direct upload into Zenobase
-	return mungeForZenobase(parseLocationHistory(filePath))
+	input = sys.argv[1]
+	output = sys.argv[2]
+	with open(output, 'w') as out:
+		print >> out, exportToZenobase(munge(parseLocationHistory(input)))
